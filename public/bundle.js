64c193310898356c5d6da64ab330dacfd8e734c4
will-ku <william.ku9192@gmail.com>
2021-07-01 11:58:16 -0400

    updated the lets get started button

--- public/bundle.js
+++ public/bundle.js
@@ -1224,7 +1224,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     for (let i = lineGraphFacts.length - 1; i > -1; i--) {
       while (lineGraphFacts[i].childNodes.length > 0) {
-        // debugger;
         lineGraphFacts[i].removeChild(lineGraphFacts[i].lastElementChild);
       }
     }
